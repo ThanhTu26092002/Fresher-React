349a3d6e1517da4207133521d9ba2b35b1e128af
ThanhTu26092002 <your_new_email@gmail.com>
2024-03-11 17:34:57 +0700

    dùng oulet tái sử dụng component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login/index.jsx";
+import { Outlet } from "react-router-dom";
+import BookPage from "./pages/book/index.jsx";
+import ContactPage from "./pages/contact/index.jsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 const Layout = () => {
-  return <>Main Page</>;
+  return (
+    <div className="layout-app">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 const router = createBrowserRouter([
@@ -11,6 +23,18 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     errorElement: <div>404 not found</div>,
+
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "book",
+        element: <BookPage />,
+      },
+    ],
   },
   {
     path: "/login",
